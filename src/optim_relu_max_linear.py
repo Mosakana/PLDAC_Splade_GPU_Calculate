d9c7699993fd7a4eb46561653b879eb768c397c3
Mosakana <wdorianliu@outlook.com>
2024-04-26 12:14:06 +0200

    try to implement triton function, but we have met a great problem.

--- src/optim_relu_max_linear.py
+++ src/optim_relu_max_linear.py
@@ -4,17 +4,129 @@ from torch.nn import ReLU
 import triton
 import triton.language as tl
 
+@triton.jit
+def gradient_w_kernel(index_ptr, grad_ptr, delta_ptr, x_ptr,
+                      stride_grad_dim_d, stride_grad_dim_v,
+                      stride_delta_dim_b, stride_delta_dim_v,
+                      stride_x_dim_b, stride_x_dim_l, stride_x_dim_d,
+                      N_INDEX, D,
+                      BLOCK_SIZE_D: tl.constexpr,
+                      BLOCK_SIZE_INDEX: tl.constexpr,
+                      GROUP_SIZE_INDEX):
+
+    ################# L2 cache optimisation ###################
+    pid = tl.program_id(axis=0)
+    num_pid_index = tl.cdiv(N_INDEX, BLOCK_SIZE_INDEX)
+    num_pid_d = tl.cdiv(D, BLOCK_SIZE_D)
+    num_pid_in_group = GROUP_SIZE_INDEX * num_pid_d
+    group_id = pid // num_pid_in_group
+    first_pid_index = group_id * GROUP_SIZE_INDEX
+    group_size_index = min(num_pid_index - first_pid_index, GROUP_SIZE_INDEX)
+    pid_index = first_pid_index + (pid % group_size_index)
+    pid_d = (pid % num_pid_in_group) // group_size_index
+    ###########################################################
+
+    offset_index = (pid_index * BLOCK_SIZE_INDEX + tl.arange(0, BLOCK_SIZE_INDEX)) % N_INDEX
+    offset_d = (pid_d * BLOCK_SIZE_D + tl.arange(0, BLOCK_SIZE_D)) % D
+
+    index_ptrs = index_ptr + offset_index
+
+    index = tl.load(index_ptrs, mask=offset_index < N_INDEX)
+
+    b, v, lmax = index
+    for test in index:
+        print(test)
+
+    grad_ptrs = grad_ptr + (offset_d[:, None] * stride_grad_dim_d + v * stride_grad_dim_v)
+
+    delta_ptr = delta_ptr + (b * stride_delta_dim_b + v * stride_delta_dim_v)
+
+    x_ptrs = x_ptr + (b * stride_x_dim_b + lmax * stride_x_dim_l, offset_d[:, None] * stride_x_dim_d)
+
+    for d in range(0, tl.cdiv(D, BLOCK_SIZE_D)):
+        grad = tl.load(grad_ptrs, mask=offset_d[:, None] < D - d * BLOCK_SIZE_D, other=0.0)
+        delta = tl.load(delta_ptr)
+        x = tl.load(x_ptrs, mask=offset_d[None, None, :] < D - d * BLOCK_SIZE_D, other=0.0)
+
+        grad += delta * x
+        tl.store(grad_ptrs, grad, mask=offset_d[:, None] < D - d * BLOCK_SIZE_D)
+
+######### forcement y'a des problemes #####################
+
+def compute_gradient_w(index, delta, x):
+    N_INDEX = len(index)
+    D = x.shape[2]
+    V = delta.shape[1]
+    grad_weight = torch.zeros(D, V, dtype=torch.float64)
+
+    grid = lambda meta: (triton.cdiv(N_INDEX, meta['BLOCK_SIZE_INDEX']) * triton.cdiv(D, meta['BLOCK_SIZE_D']), )
+
+    gradient_w_kernel[grid](index, grad_weight, delta, x,
+                            grad_weight.stride(0), grad_weight.stride(1),
+                            delta.stride(0), delta.stride(1),
+                            x.stride(0), x.stride(1), x.stride(2),
+                            N_INDEX, D,
+                            BLOCK_SIZE_D=32, BLOCK_SIZE_INDEX=1,
+                            GROUP_SIZE_INDEX=8)
+
+    return grad_weight
+
+# def gradient_w_kernel(index_ptr, grad_ptr, delta_ptr, x_ptr,
+#                       stride_index_dim_b, stride_index_dim_v,
+#                       stride_grad_dim_d, stride_grad_dim_v,
+#                       stride_delta_dim_b, stride_delta_dim_v,
+#                       stride_x_dim_b, stride_x_dim_l, stride_x_dim_d,
+#                       B, V, D,
+#                       BLOCK_SIZE_B: tl.constexpr,
+#                       BLOCK_SIZE_V: tl.constexpr,
+#                       BLOCK_SIZE_D: tl.constexpr,
+#                       GROUP_SIZE_V: tl.constexpr):
+#
+#     pid = tl.program_id(axis=0)
+#
+#     ################# L2 cache optimisation ###################
+#     num_pid_v = tl.cdiv(V, BLOCK_SIZE_V)
+#     num_pid_b = tl.cdiv(B, BLOCK_SIZE_B)
+#
+#     num_pid_in_group = GROUP_SIZE_V * num_pid_b
+#     group_id = pid // num_pid_in_group
+#     first_pid_v = group_id * GROUP_SIZE_V
+#     group_size_v = min(num_pid_v - first_pid_v, GROUP_SIZE_V)
+#     pid_v = first_pid_v + (pid % group_size_v)
+#     pid_b = (pid % num_pid_in_group) // group_size_v
+#     ###########################################################
+#
+#     offset_v = (pid_v * BLOCK_SIZE_V + tl.arange(0, BLOCK_SIZE_V)) % V
+#     offset_b = (pid_b * BLOCK_SIZE_B + tl.arange(0, BLOCK_SIZE_B)) % B
+#     offset_d = tl.arange(0, BLOCK_SIZE_D)
+#
+#     index_ptrs = index_ptr + (offset_b[:, None] * stride_index_dim_b + offset_v[None, :] * stride_index_dim_v)
+#
+#     pass
+#
+def gradient_x_kernel(index_ptr, grad_ptr, delta_ptr, x_ptr,
+                      stride_index_dim_b, stride_index_dim_v,
+                      stride_grad_dim_d, stride_grad_dim_v,
+                      stride_delta_dim_b, stride_delta_dim_v,
+                      stride_x_dim_b, stride_x_dim_l, stride_x_dim_d,
+                      B, V, D,
+                      BLOCK_SIZE_B: tl.constexpr,
+                      BLOCK_SIZE_V: tl.constexpr,
+                      BLOCK_SIZE_D: tl.constexpr,
+                      GROUP_SIZE_V: tl.constexpr):
+
+    pid = tl.program_id(axis=0)
+
+    start_block_b = pid * BLOCK_SIZE_B
+
+    offset_b = start_block_b + tl.arange(0, BLOCK_SIZE_B)
+    offset_v = tl.arange(0, V)
+
+
+
+
+
 class OptimReluMaxLinear(Function):
-    def gradient_x_kernel(self, index_ptr, output_ptr, output_row_stride, BLOCK_SIZE:tl.constexpr):
-        # divide on B
-        pid = tl.program_id(axis=0)
-
-
-
-
-
-
-
     @staticmethod
     def forward(x, weight, bias, mask):
         '''
@@ -53,4 +165,11 @@ class OptimReluMaxLinear(Function):
 
     @staticmethod
     def backward(ctx, *grad_outputs):
+        x, weight, bias, effective_indice = ctx.saved_tensors
+        grad_x = grad_weight = grad_bias = None
+
+        if ctx.needs_input_grad[1]:
+            grad_weight = compute_gradient_w(effective_indice, grad_outputs[0], x)
+
+        return grad_x, grad_weight, grad_bias, None
 
--- src/plateform_test_GPU.py
+++ src/plateform_test_GPU.py
@@ -1,59 +1,47 @@
 import triton
 import triton.language as tl
 import torch
+from optim_relu_max_linear import OptimReluMaxLinear
+from copy import deepcopy
+from torch.nn import ReLU
 
-@triton.jit
-def add_kernel(x_ptr,  # *Pointer* to first input vector.
-               y_ptr,  # *Pointer* to second input vector.
-               output_ptr,  # *Pointer* to output vector.
-               n_elements,  # Size of the vector.
-               BLOCK_SIZE: tl.constexpr,  # Number of elements each program should process.
-               # NOTE: `constexpr` so it can be used as a shape value.
-               ):
-    # There are multiple 'programs' processing different data. We identify which program
-    # we are here:
-    pid = tl.program_id(axis=0)  # We use a 1D launch grid so axis is 0.
-    # This program will process inputs that are offset from the initial data.
-    # For instance, if you had a vector of length 256 and block_size of 64, the programs
-    # would each access the elements [0:64, 64:128, 128:192, 192:256].
-    # Note that offsets is a list of pointers:
-    block_start = pid * BLOCK_SIZE
-    offsets = block_start + tl.arange(0, BLOCK_SIZE)
-    # Create a mask to guard memory operations against out-of-bounds accesses.
-    mask = offsets < n_elements
-    # Load x and y from DRAM, masking out any extra elements in case the input is not a
-    # multiple of the block size.
-    x = tl.load(x_ptr + offsets, mask=mask)
-    y = tl.load(y_ptr + offsets, mask=mask)
-    output = x + y
-    # Write x + y back to DRAM.
-    tl.store(output_ptr + offsets, output, mask=mask)
+batch = 32
+length = 10
+data_size = 32
 
-def add(x: torch.Tensor, y: torch.Tensor):
-    # We need to preallocate the output.
-    output = torch.empty_like(x)
-    assert x.is_cuda and y.is_cuda and output.is_cuda
-    n_elements = output.numel()
-    # The SPMD launch grid denotes the number of kernel instances that run in parallel.
-    # It is analogous to CUDA launch grids. It can be either Tuple[int], or Callable(metaparameters) -> Tuple[int].
-    # In this case, we use a 1D grid where the size is the number of blocks:
-    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
-    # NOTE:
-    #  - Each torch.tensor object is implicitly converted into a pointer to its first element.
-    #  - `triton.jit`'ed functions can be indexed with a launch grid to obtain a callable GPU kernel.
-    #  - Don't forget to pass meta-parameters as keywords arguments.
-    add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
-    # We return a handle to z but, since `torch.cuda.synchronize()` hasn't been called, the kernel is still
-    # running asynchronously at this point.
-    return output
+vocabulary = 20
 
-torch.manual_seed(0)
-size = 98432
-x = torch.rand(size, device='cuda')
-y = torch.rand(size, device='cuda')
-output_torch = x + y
-output_triton = add(x, y)
-print(output_torch)
-print(output_triton)
-print(f'The maximum difference between torch and triton is '
-      f'{torch.max(torch.abs(output_torch - output_triton))}')
+list_lengths = torch.randint(0, length, [batch])
+
+
+x = torch.randn(batch, length, data_size, requires_grad=True, dtype=torch.float64).cuda()
+w = torch.randn(data_size, vocabulary, requires_grad=True, dtype=torch.float64).cuda()
+b = torch.randn(vocabulary, requires_grad=True, dtype=torch.float64).cuda()
+
+mask = torch.ones(batch, length).cuda()
+for i, l in enumerate(list_lengths):
+    mask[i, l:] = 0
+
+splade = OptimReluMaxLinear.apply(x, w, b, mask)
+
+splade[0].sum().backward()
+
+grad_splade_w = deepcopy(w.grad)
+
+x.grad = None
+w.grad = None
+b.grad = None
+
+mask = torch.where(mask == 1, 0, -torch.inf)
+
+relu = ReLU()
+
+out = x @ w
+out += b.reshape(1, 1, *b.shape)
+out += mask.reshape(*mask.shape, 1)
+out = relu(torch.max(out, 1)[0])
+
+
+out.sum().backward()
+
+print(torch.all(grad_splade_w.isclose(w.grad)))
